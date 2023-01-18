a2004315cfe6fefcb264d0297f97fc761bdacf9c
Ashwin Jose <ajashwin8880@gmail.com>
2023-01-19 00:49:05 +0530

    Added JS Logic for heading

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const heading = document.querySelector(".heading");
+heading.addEventListener("mouseover", function(event) {
+    heading.style.fontSize ="50px"; //fontsize of header gets increased to 50px
+})
