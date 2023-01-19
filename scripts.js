e94c2e05c2c79e130351dcc25e0cada80f3cc0e9
Ashwin Jose <ajashwin8880@gmail.com>
2023-01-19 12:27:50 +0530

    kept relative unit on hover

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 const heading = document.querySelector(".heading");
 heading.addEventListener("mouseover", function(event) {
-    heading.style.fontSize ="50px"; //fontsize of header gets increased to 50px
+    heading.style.fontSize ="3.5em"; //fontsize of header gets increased to 50px
 })
